80d01ce4e34e64b145e2f2a9719c4b7cb58487a9
PorchDriver <tantoniw@cogeco.ca>
2018-11-07 11:57:08 -0500

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,71 @@
-//Initialize topics
+//Initialize data
 var downloadNumber = 10;
-
 var topics = ["porsche", "volkswagen", "bmw", "audi"];
-
-//Create buttons
-function buttonCreator() {
-$("#button-bar").text("");
-    for (i=0; i<topics.length; i++) {
-    $("#button-bar").append("<button id=choice class='btn btn-secondary' value=" + topics[i] + ">" + topics[i] + "</button><br><br>");
-    console.log(topics[i]);
-};
-};
+var downloadIndex = [1,1,1,1];
+//Start application
 buttonCreator();
+
 $(document).on('click', '.btn', function() {
-    console.log("You clicked", $(this).val())
+    //console.log("You clicked", $(this).val())
     target = $(this).val();
-    console.log(target);
-
-
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-target + "&api_key=dc6zaTOxFJmzC&limit="+downloadNumber;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-      console.log(response);
-      results = response.data
-      for (i=0; i<10; i++) {
-        $photoDiv = $("<div>");
-        $p = $("<p>");
-        $p.text("Rating: "+results[i].rating);
-        $image = $("<img>");
-        $image.attr("src",results[i].images.fixed_height_still.url);
-        $image.attr("data-still",results[i].images.fixed_height_still.url);
-        $image.attr("data-animate",results[i].images.fixed_height.url);
-        $image.attr("data-state","still");
-        $image.attr("class","gif");
-        $photoDiv.append($image);
-        $photoDiv.append($p);
-        $("#photos-appear-here").prepend($photoDiv);
+    //console.log(target);
+    for (j=0; j<topics.length; j++) {
+      //console.log("j "+j);
+      if (target === topics[j]) {
+        //console.log ("topics "+topics[j]);
+        var downloadMult = downloadIndex[j];
+        //console.log("index "+ downloadIndex);
+        var downloadTotal = downloadMult*downloadNumber;
+        var downloadStart = (downloadMult-1)*downloadNumber;
+        //console.log(downloadTotal);
+         downloadIndex[j] ++;
+         //console.log("updated index "+downloadIndex);
       }
+    } 
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    target + "&api_key=C11J3fVoCqwB3kPXS1507oAHLWUAytxc&limit="+downloadTotal;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+      }).then(function(response) {
+        //console.log(response);
+        results = response.data
+        for (i=downloadStart; i<(downloadTotal); i++) {
+          $photoDiv = $("<div>");
+          $p = $("<p>");
+          $p.text("Title: "+results[i].title);
+          $p.append("<br>Rating: "+results[i].rating);
+          $image = $("<img>");
+          $image.attr("src",results[i].images.fixed_height_still.url);
+          $image.attr("data-still",results[i].images.fixed_height_still.url);
+          $image.attr("data-animate",results[i].images.fixed_height.url);
+          $image.attr("data-state","still");
+          $image.attr("class","gif");
+          $photoDiv.append($image);
+          $photoDiv.append($p);
+          $("#photos-appear-here").prepend($photoDiv);
+        }
+    });
+
   });
-
-});
-
+//Add another car maufucatuere routine
 $("#add-car").on("click", function(event) {
     event.preventDefault();
     var carInput = $("#car-input").val().trim();
-
     topics.push(carInput);
-
+    downloadIndex.push(1);
+    //console.log(downloadIndex);
     buttonCreator();
   });
-
+//Stop or animate the gif toggle
   $(document).on("click",".gif", function() {
-    console.log(this);
+    //console.log(this);
     var $image = $(this);
-    console.log($image);
+    //console.log($image);
       var state = $image.attr("data-state");
-      console.log(state);
+      //console.log(state);
       if (state === "still") {
         value = $(this).attr("data-animate");
         $(this).attr("src",value);
@@ -71,3 +77,12 @@ $("#add-car").on("click", function(event) {
         $(this).attr("data-state","still");
       }
   });
+//Button creator function
+  function buttonCreator() {
+    $("#button-bar").text("");
+        for (i=0; i<topics.length; i++) {
+        $("#button-bar").append("<button id=choice class='btn btn-secondary' value=" + topics[i] + ">" + topics[i] + "</button><br><br>");
+        //console.log(topics[i]);
+    };
+    };
+    
